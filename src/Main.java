fe29274995a8581cbad1ef1dfe01b9b5b0543e81
abrart4 <abrart4@nycstudents.net>
2025-11-09 17:46:08 -0500

    Implement challenge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         WordMatch game;
 
         game = new WordMatch("mississippi");
@@ -22,5 +28,29 @@ public class Main {
         System.out.println(game.scoreGuess("con"));
         System.out.println(game.scoreGuess("cat"));
         System.out.println(game.findBetterGuess("con", "cat"));
+
+        System.out.println(read());
+    }
+
+    public static int read() throws FileNotFoundException {
+        int totalScore = 0;
+
+        File guessesFile = new File("Guesses.txt");
+        Scanner guessesScanner = new Scanner(guessesFile);
+        while (guessesScanner.hasNextLine()) {
+            String word = guessesScanner.next();
+            String firstGuess = guessesScanner.next();
+            String secondGuess = guessesScanner.next();
+
+            WordMatch game = new WordMatch(word);
+            String betterGuess = game.findBetterGuess(firstGuess, secondGuess);
+            int scoreOfBetterGuess = game.scoreGuess(betterGuess);
+            totalScore += scoreOfBetterGuess;
+            /*
+            WordMatch game = new WordMatch(guessesScanner.next());
+            totalScore += game.scoreGuess(game.findBetterGuess(guessesScanner.next(), guessesScanner.next()));
+            */
+        }
+        return totalScore;
     }
 }
